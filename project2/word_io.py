b2e11f5dd9ace145213cbed9b6742e8904a14732
ANIK-01 <anikmd445@gmail.com>
2024-10-24 21:51:11 +0600

    2 game project added

---
+++ project2/word_io.py
@@ -0,0 +1,24 @@
+word_dict = {
+    "fruits": [
+        "apple", "banana", "orange", "mango", "grape", "watermelon", "pineapple", 
+        "strawberry", "blueberry", "kiwi", "papaya", "pear", "peach", "plum", 
+        "pomegranate", "guava", "cherry", "blackberry", "fig", "lemon"
+    ],
+    "animals": [
+        "dog", "cat", "elephant", "lion", "tiger", "giraffe", "zebra", "cheetah", 
+        "kangaroo", "panda", "bear", "wolf", "deer", "rabbit", "fox", "hippo", 
+        "rhino", "monkey", "cow", "horse"
+    ],
+    "flowers": [
+        "rose", "tulip", "daisy", "sunflower", "lily", "orchid", "jasmine", 
+        "lotus", "marigold", "lavender", "daffodil", "hibiscus", "cherry blossom", 
+        "violet", "poppy", "geranium", "peony", "magnolia", "iris", "freesia"
+    ],
+    "country": [
+        "United States", "Canada", "India", "China", "Brazil", "Australia", "Japan", 
+        "Germany", "France", "Mexico", "Russia", "Italy", "South Africa", "United Kingdom", 
+        "Argentina", "Spain", "Sweden", "South Korea", "Egypt", "New Zealand"
+    ]
+}
+
+
